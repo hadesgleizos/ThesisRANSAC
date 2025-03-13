ad25c213b7d28341bcd5d4f61e31ef1492b7b753
aethrna <166973346+hadesgleizos@users.noreply.github.com>
2025-03-13 14:37:47 +0800

    persisting sensitivity slider from main menu

--- Assets/Scripts/UI/MainMenuManager.cs
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -1,8 +1,29 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
+    public Slider sensitivitySlider;
+
+    void Start()
+    {
+        if (sensitivitySlider != null)
+        {
+            sensitivitySlider.minValue = 1f;
+            sensitivitySlider.maxValue = 100f;
+            float savedSensitivity = PlayerPrefs.GetFloat("MouseSensitivity", 30f);
+            sensitivitySlider.value = savedSensitivity;
+            sensitivitySlider.onValueChanged.AddListener(UpdateSensitivity);
+        }
+    }
+
+    private void UpdateSensitivity(float value)
+    {
+        PlayerPrefs.SetFloat("MouseSensitivity", value);
+        PlayerPrefs.Save();
+    }
+
     // Call this from a button OnClick() or anywhere you want to start the game.
     public void StartGame()
     {
